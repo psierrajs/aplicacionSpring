3eb9177f89dcc7f9f7879b00042611b164e514e0
psierrajs <psierra.js@gmail.com>
2024-03-24 11:08:30 +0100

    Project creation

---
+++ demo/src/test/java/com/aplicacion/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aplicacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
